2ab5c5db34ebc7db13c7a8c2acf1bbe11443318c
JamesonStansfield <jameson.stansfield@stu.ocsb.ca>
2024-03-07 16:47:57 -0500

    print button

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("pc-specs").innerHTML = "Above is a diagram found of the Atanasoff-Berry Computer (ABC). The computer was packed with more than 300 vacume tubes with the cpu running at 60hz. The computer has 3000 bits of memoryand weighs a total of 700 pounds."
+function printPage() {
+    window.print();
+}
